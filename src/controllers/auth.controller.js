514a9a7bf435e44358a6ab5846c804a8c7596c57
Arka <arkapravasantra17@gmail.com>
2025-08-10 22:24:30 +0530

    Mindit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,33 @@
-const userModel = require('../models/user.model')
-
+const userModel = require("../models/user.model");
+const bcrypt = require('bcryptjs')
 
 async function getRegisterController(req, res) {
   res.render("register");
 }
 async function postRegisterController(req, res) {
- const {username, email, password} = req.body
+  const { username, email, password } = req.body;
+  const isUserExists = await userModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+  if (isUserExists) {
+    return res.status(400).json({
+      message: "User Already exists with this this username and Email",
+    });
+  }
+  const hashPassword = await bcrypt.hash(password,10)
+
+  const user = await userModel.create({
+    username:username,
+    email:email,
+    password:hashPassword
+  })
+  return res.status(201).json({
+    message:"user register Successfully",
+    user
+  })
 }
 
 module.exports = {
   getRegisterController,
-  postRegisterController
+  postRegisterController,
 };
