21d015cd7c353ca6234bba760638579a1c29ea43
Arka <arkapravasantra17@gmail.com>
2025-08-10 12:10:32 +0530

    Matter

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,14 @@
+const userModel = require('../models/user.model')
+
+
 async function getRegisterController(req, res) {
   res.render("register");
+}
+async function postRegisterController(req, res) {
+ const {username, email, password} = req.body
 }
 
 module.exports = {
   getRegisterController,
+  postRegisterController
 };
